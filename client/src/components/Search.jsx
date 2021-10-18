cb851f2836c73335c0839f77539446f340e32402
Arun Bhardwaj <arun.b.95@gmail.com>
2021-10-18 13:01:33 -0400

    frontend bugs fixed

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,6 +6,8 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+
+    this.onChange = this.onChange.bind(this);
   }
 
   onChange (e) {
@@ -16,14 +18,17 @@ class Search extends React.Component {
 
   search() {
     this.props.onSearch(this.state.term);
+    this.setState({
+      term: '',
+    })
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
-    </div>) 
+      Enter a github username: <input value={this.state.term} onChange={this.onChange}/>
+      <button onClick={() => this.search()}> Add Repos </button>
+    </div>)
   }
 }
 
--- database/index.js
+++ database/index.js
@@ -3,14 +3,31 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: String,
+  repo_id: Number,
+  description: String,
+  owner: String,
+  owner_id: Number,
+  watchers: Number,
+  forks: Number,
+  size: Number,
+  private: Boolean,
+  date: {
+    created_at: String,
+    updated_at: String,
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+// Make sure to format repoObject
+let save = (repoObject = {}) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let newRepo = new Repo(repoObject);
+  // await
+          newRepo.save();
 }
 
 module.exports.save = save;
